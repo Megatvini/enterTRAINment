ac7e5b662928e58cf42503d05daf7b6fb1dcfe99
Rshalika <rshal13@freeuni.edu.ge>
2016-07-24 03:29:54 +0400

    add Adapter interface

---
+++ app/src/main/java/ge/edu/freeuni/android/entertrainment/music/MusicAdapter.java
@@ -0,0 +1,9 @@
+package ge.edu.freeuni.android.entertrainment.music;
+
+import java.util.List;
+
+import ge.edu.freeuni.android.entertrainment.music.data.Song;
+
+public interface MusicAdapter {
+    public void setData(List<Song> songs);
+}
