7f6915a47846ad471aa414833debc65aa32f11b4
Nika Doghonadze <ndogh13@freeuni.edu.ge>
2016-07-24 20:27:40 +0400

    Add animations for reading

--- app/src/main/java/ge/edu/freeuni/android/entertrainment/reading/ReadingFragment.java
+++ app/src/main/java/ge/edu/freeuni/android/entertrainment/reading/ReadingFragment.java
@@ -4,8 +4,10 @@ package ge.edu.freeuni.android.entertrainment.reading;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ViewFlipper;
 
 import ge.edu.freeuni.android.entertrainment.R;
 
@@ -13,6 +15,10 @@ import ge.edu.freeuni.android.entertrainment.R;
  * A simple {@link Fragment} subclass.
  */
 public class ReadingFragment extends Fragment {
+    private ViewFlipper viewFlipper;
+    private float lastX;
+
+    private static int NUM_CHILDREN = 3;
 
 
     public ReadingFragment() {
@@ -24,7 +30,44 @@ public class ReadingFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reading, container, false);
+        View view =  inflater.inflate(R.layout.fragment_reading, container, false);
+
+        viewFlipper = (ViewFlipper) view.findViewById(R.id.reading_view_flipper);
+        viewFlipper.setDisplayedChild(0);
+        initTouchListener(view);
+
+        return view;
+    }
+
+    private void initTouchListener(View view) {
+        view.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch (motionEvent.getAction()) {
+                    case MotionEvent.ACTION_DOWN: {
+                        lastX = motionEvent.getX();
+                        break;
+                    }
+                    case MotionEvent.ACTION_UP: {
+                        float currentX = motionEvent.getX();
+
+                        if (lastX < currentX) {
+                            viewFlipper.setInAnimation(getContext(), R.anim.in_from_left);
+                            viewFlipper.setOutAnimation(getContext(), R.anim.out_to_right);
+                            viewFlipper.showPrevious();
+                        }
+
+                        if (lastX > currentX) {
+                            viewFlipper.setInAnimation(getContext(), R.anim.in_from_right);
+                            viewFlipper.setOutAnimation(getContext(), R.anim.out_to_left);
+                            viewFlipper.showNext();
+                        }
+                        break;
+                    }
+                }
+                return true;
+            }
+        });
     }
 
 }
