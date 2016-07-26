fbb57a831bcdd766648e6da076232bb98ca54adf
Rshalika <rshal13@freeuni.edu.ge>
2016-07-27 02:05:37 +0400

    visual changes

--- app/src/main/java/ge/edu/freeuni/android/entertrainment/music/offered/OfferedMusicsFragment.java
+++ app/src/main/java/ge/edu/freeuni/android/entertrainment/music/offered/OfferedMusicsFragment.java
@@ -45,15 +45,16 @@ public class OfferedMusicsFragment extends Fragment implements OnListFragmentInt
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.offered_music_fragment_item_list, container, false);
-        if (view instanceof RecyclerView) {
-            Context context = view.getContext();
-            RecyclerView recyclerView = (RecyclerView) view;
-            recyclerView.setLayoutManager(new LinearLayoutManager(context));
-            recyclerView.setAdapter(adapter);
-        }
+        View parentView = inflater.inflate(R.layout.offered_music_fragment_item_list, container, false);
+
+        Context context = parentView.getContext();
+
+        RecyclerView recyclerView = (RecyclerView)parentView.findViewById(R.id.list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(context));
+        recyclerView.setAdapter(adapter);
+
         initAdapterAndProvider();
-        return view;
+        return parentView;
     }
 
 
